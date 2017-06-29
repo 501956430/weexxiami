327b06366a6e5f17162fe53248665c3530f17eba
tangbo <501956430@qq.com>
2017-06-29 18:47:08 +0800

    消息页

--- src/msg.vue
+++ src/msg.vue
@@ -1,5 +1,8 @@
 <template>
-    <scroller style="background-color: #F8F8F8">
+    <scroller style="background-color: #F8F8F8;" append="tree">
+        <refresh class="refresh-view" :display="refresh_display" @refresh="onrefresh">
+            <img id="roate" ref="roate" src="http://gw.alicdn.com/bao/uploaded/TB1xDrVNFXXXXbEXFXXXXXXXXXX-48-48.png" style="width: 50px;height: 50px;"></img>
+        </refresh>
         <div style="background-color: white">
             <div style="height: 150;flex-direction:row;justify-content:center;align-items:center;border-bottom-color: #EEEEEE;border-bottom-width: 1;">
                 <div style="flex: 0.3;margin-left: 20;">
@@ -39,19 +42,37 @@
             </div>
         </div>
     </scroller>
-    </div>
 </template>
 
 <script>
     var api = require('./api.js');
     export default {
         data: {
+            refresh_display: 'hide',
             path:api.apiurl.path,
         },
-        methods: {}
+        methods: {
+            onrefresh: function(e) {
+                var self = this;
+                self.refresh_display = 'show';
+                setTimeout(function () {
+                    self.refresh_display = 'hide';
+                }, 3000)
+            }
+        }
     }
 </script>
 
 <style>
-
+    .refresh-view {
+        height: 120px;
+        width: 750px;
+        display: -ms-flex;
+        display: -webkit-flex;
+        display: flex;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        align-items: center;
+    }
 </style>
